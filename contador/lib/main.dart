a160303c50935df0ae98be5abcbe580e32a936fe
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-09-25 11:23:46 -0500

    APP CONTADOR

---
+++ contador/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:contador/src/app.dart';
+
+// import 'src/app.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+
+  // correr aplicacion
+  runApp( MyApp() );
+
+}
---
+++ contador/lib/src/pages/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatelessWidget{
+
+  final estiloTexto = new TextStyle( fontSize: 25 );
+
+  @override
+  Widget build( BuildContext context ) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Titulo'),
+        centerTitle: true,
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Cantidad de clicks: ', style: estiloTexto),
+            Text('0', style: estiloTexto),
+          ],
+        )
+      ),
+    )
+  }
+
+}
