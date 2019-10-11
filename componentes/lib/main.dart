8bb5f526235bcf60347d4ffd16046f1c3d5b8e4f
Samuel Ramirez <samuel_ramirez@live.com.mx>
2019-10-10 21:34:02 -0500

    02 Componentes App | Primeros componentes

---
+++ componentes/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/home_page.dart';
+ 
+
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      // home: HomePageTemp()
+      home: HomePage()
+    );
+  }
+}
